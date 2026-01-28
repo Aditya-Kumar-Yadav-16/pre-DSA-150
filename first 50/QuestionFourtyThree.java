18fbc6d72b27b88ccda842c9c28cd65d2dbefa45
Aditya-Kumar-Yadav-16 <221833157+Aditya-Kumar-Yadav-16@users.noreply.github.com>
2026-01-28 23:15:06 +0530

    43

---
+++ first 50/QuestionFourtyThree.java
@@ -0,0 +1,23 @@
+// Finding All Divisors of a Number
+// Difficulty: Easy
+// Topics: Basic Programming, Mathematical Computations
+// Description: Write a program to find all divisors of a given number.
+// Example:
+// Input: number = 12
+// Output: 1, 2, 3, 4, 6, 12
+// Explanation: The divisors of 12 are 1, 2, 3, 4, 6, and 12.
+
+import java.util.*;
+class QuestionFourtyThree {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+        System.out.print("Divisors of " + number + " are: ");
+        for(int i = 1; i <= number; i++){
+            if(number % i == 0){
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
